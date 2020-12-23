b09f656d700a9e44587b47a30447e337db7f0741
bhsuhkr <bhsuhkr@gmail.com>
2020-12-22 21:17:21 -0600

    Add weather text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude,callback) => {
         }else if(body.error){
             callback('Unable to find location.', undefined)
         }else{
-            callback(undefined, body.current.weather_descriptions[0]+'. It\'s currently '+body.current.temperature+ ' degree.')
+            callback(undefined, body.current.weather_descriptions[0]+'. It\'s currently '+body.current.temperature+ ' degree, and feels like ' + body.current.feelslike + ' degree. The humidity is ' + body.current.humidity + '%.')
         }
     })
 }
